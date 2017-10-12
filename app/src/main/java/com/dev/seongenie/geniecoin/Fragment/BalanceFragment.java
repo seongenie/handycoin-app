012ffdcf247ed3e375bcd5dfc022ef39ff1b3d9b
seongjin <lsj234027@gmail.com>
2017-10-12 23:23:32 +0900

    잔고 레이아웃 개발

--- app/src/main/java/com/dev/seongenie/geniecoin/Fragment/BalanceFragment.java
+++ app/src/main/java/com/dev/seongenie/geniecoin/Fragment/BalanceFragment.java
@@ -4,39 +4,29 @@ package com.dev.seongenie.geniecoin.Fragment;
  * Created by seongjinlee on 2017. 10. 2..
  */
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
 
-import com.dev.seongenie.geniecoin.Fragment.Balance.IconTreeItemHolder;
-import com.dev.seongenie.geniecoin.Fragment.Balance.ProfileHolder;
-import com.dev.seongenie.geniecoin.Fragment.Balance.SelectableHeaderHolder;
-import com.dev.seongenie.geniecoin.Fragment.Balance.SelectableItemHolder;
+import com.dev.seongenie.geniecoin.Fragment.OrderBook.AnimatedExpandableListView;
+import com.dev.seongenie.geniecoin.Fragment.OrderBook.AnimatedExpandableListView.AnimatedExpandableListAdapter;
 import com.dev.seongenie.geniecoin.R;
 import com.dev.seongenie.geniecoin.ServerConnection.BithumbPrivate.Api_Client;
 import com.dev.seongenie.geniecoin.ServerConnection.BithumbPrivate.BalanceView;
 import com.dev.seongenie.geniecoin.ServerConnection.BithumbPrivate.BithumbRequest;
-import com.unnamed.b.atv.model.TreeNode;
-import com.unnamed.b.atv.view.AndroidTreeView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
-
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-
-import com.unnamed.b.atv.model.TreeNode;
-import com.unnamed.b.atv.view.AndroidTreeView;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -48,12 +38,14 @@ import retrofit2.Response;
  */
 public class BalanceFragment extends Fragment {
 
-    private AndroidTreeView tView;
-    private boolean selectionModeEnabled = false;
     private HashMap<String, Double> balanceData;
-    TreeNode root;
     private String apiKey = "24ab10d0a3b68a8ffc68429e19894aaf";
     private String secretKey = "c3baffe3380a1a5f8263289997ff95c7";
+    private Callback<BalanceView> callback;
+    private AnimatedExpandableListView listView;
+    private BalaceAdapter adapter;
+
+    private int previousGroup=-1;
 
     public BalanceFragment() {
         // Required empty public constructor
@@ -63,14 +55,7 @@ public class BalanceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
-
-        String endPoint = "/info/balance";
-        HashMap<String, String> rgParams = new HashMap<String, String>();
-        rgParams.put("currency", "ALL");
-
-        HashMap<String, String>[] headerParams = new Api_Client(apiKey, secretKey).getHeaders(endPoint, rgParams);
-        BithumbRequest.getInstance().getBalances(headerParams[0], headerParams[1], new Callback<BalanceView>() {
+        callback = new Callback<BalanceView>() {
             @Override
             public void onResponse(Call<BalanceView> call, Response<BalanceView> response) {
 
@@ -97,29 +82,206 @@ public class BalanceFragment extends Fragment {
             public void onFailure(Call<BalanceView> call, Throwable t) {
                 Log.i("seongenie", "receive failed");
             }
+        };
+        View v = inflater.inflate(R.layout.fragment_balance, null, false);
+
+        List<GroupExchange> items = new ArrayList<GroupExchange>();
+
+        GroupExchange item = new GroupExchange();
+
+        item.exchange = "빗썸";
+
+        BalanceCoin child = new BalanceCoin();
+        child.coinName = "리플";
+        item.items.add(child);
+
+        child = new BalanceCoin();
+        child.coinName = "비트코인 캐시";
+        item.items.add(child);
+
+        items.add(item);
+
+        item = new GroupExchange();
+
+        item.exchange = "코인원";
+
+        child = new BalanceCoin();
+        child.coinName = "비트코인";
+        item.items.add(child);
+
+        child = new BalanceCoin();
+        child.coinName = "대시";
+        item.items.add(child);
+
+        items.add(item);
+
+        adapter = new BalaceAdapter(getContext());
+        adapter.setData(items);
+
+        listView = (AnimatedExpandableListView) v.findViewById(R.id.listview_balance);
+        listView.setAdapter(adapter);
+
+        // In order to show animations, we need to use a custom click handler
+        // for our ExpandableListView.
+        listView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                // We call collapseGroupWithAnimation(int) and
+                // expandGroupWithAnimation(int) to animate group
+                // expansion/collapse.
+                if (listView.isGroupExpanded(groupPosition)) {
+                    listView.collapseGroupWithAnimation(groupPosition);
+                } else {
+                    listView.expandGroupWithAnimation(groupPosition);
+                }
+                return true;
+            }
+
         });
 
-        View rootView = inflater.inflate(R.layout.fragment_balance, null, false);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        return rootView;
+        return v;
     }
 
     private void setMyBalance(HashMap<String, Double> data) {
         this.balanceData = data;
     }
+
+
+    private void requestBalance() {
+        String endPoint = "/info/balance";
+        HashMap<String, String> rgParams = new HashMap<String, String>();
+        rgParams.put("currency", "ALL");
+
+        HashMap<String, String>[] headerParams = new Api_Client(apiKey, secretKey).getHeaders(endPoint, rgParams);
+        BithumbRequest.getInstance().getBalances(headerParams[0], headerParams[1], callback);
+    }
+
+
+    private static class GroupExchange {
+        String exchange;
+        List<BalanceCoin> items = new ArrayList<BalanceCoin>();
+    }
+
+    private static class BalanceCoin {
+        String exchangeName;
+        String coinName;
+        double qnty;
+    }
+
+    private static class ChildHolder {
+        TextView title;
+    }
+
+    private static class GroupHolder {
+        TextView title;
+    }
+
+
+    /**
+     * Adapter for our list of {@link GroupExchange}s.
+     */
+    private class BalaceAdapter extends AnimatedExpandableListAdapter {
+        private LayoutInflater inflater;
+
+        private List<GroupExchange> exchanges;
+
+        public BalaceAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+        }
+
+        public void setData(List<GroupExchange> exchanges) {
+            this.exchanges = exchanges;
+        }
+
+        @Override
+        public BalanceCoin getChild(int groupPosition, int childPosition) {
+            return exchanges.get(groupPosition).items.get(childPosition);
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        @Override
+        public View getRealChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            ChildHolder holder;
+            BalanceCoin item = getChild(groupPosition, childPosition);
+            if (convertView == null) {
+                holder = new ChildHolder();
+                convertView = inflater.inflate(R.layout.balance_list_item, parent, false);
+                holder.title = (TextView) convertView.findViewById(R.id.lblListItem);
+                convertView.setTag(holder);
+            } else {
+                holder = (ChildHolder) convertView.getTag();
+            }
+
+            holder.title.setText(item.coinName);
+
+            return convertView;
+        }
+
+        @Override
+        public int getRealChildrenCount(int groupPosition) {
+            return exchanges.get(groupPosition).items.size();
+        }
+
+        @Override
+        public GroupExchange getGroup(int groupPosition) {
+            return exchanges.get(groupPosition);
+        }
+
+        @Override
+        public int getGroupCount() {
+            return exchanges.size();
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            GroupHolder holder;
+            GroupExchange item = getGroup(groupPosition);
+            if (convertView == null) {
+                holder = new GroupHolder();
+                convertView = inflater.inflate(R.layout.header_balance, parent, false);
+                holder.title = (TextView) convertView.findViewById(R.id.lblListHeader);
+                convertView.setTag(holder);
+            } else {
+                holder = (GroupHolder) convertView.getTag();
+            }
+
+            holder.title.setText(item.exchange);
+
+            return convertView;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public boolean isChildSelectable(int arg0, int arg1) {
+            return true;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
